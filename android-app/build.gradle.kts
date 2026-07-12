3b51a496d9316560ff7b98881651a5f696971e27
hank2630 <hankz2630@gmail.com>
2026-07-12 14:38:20 +0800

    Update build.gradle.kts with AGP 8.1.0 and remove problematic dependencies

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.2.0"
+    id("com.android.application") version "8.1.0"
     kotlin("android") version "1.9.21"
     kotlin("plugin.serialization") version "1.9.21"
 }
@@ -74,9 +74,6 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-
-    // YouTube Player - 使用 WebView 實現替代方案
-    // 移除: implementation("com.pierfrancescosoffritti:android-youtube-player:11.0.1")
 
     // Image loading
     implementation("io.coil-kt:coil-compose:2.4.0")
