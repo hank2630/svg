87bd04892d9f7bcd6dd03dd490202e5c31bcd653
hank2630 <hankz2630@gmail.com>
2026-07-12 14:36:15 +0800

    Downgrade AGP to 8.1.0 for compatibility

--- android-app/settings.gradle.kts
+++ android-app/settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
     }
     
     plugins {
-        id("com.android.application") version "8.2.0"
-        id("com.android.library") version "8.2.0"
+        id("com.android.application") version "8.1.0"
+        id("com.android.library") version "8.1.0"
         kotlin("android") version "1.9.21"
         kotlin("jvm") version "1.9.21"
         kotlin("plugin.serialization") version "1.9.21"
